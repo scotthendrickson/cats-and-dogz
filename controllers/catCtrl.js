6132b5c9eb32c6de1b490ca1e5e8033bc3949f85
Nick Marucci <npm1514@gmail.com>
2016-04-21 11:36:07 -0600

    finished app, now I need to create initial docs and readme

--- controllers/catCtrl.js
+++ controllers/catCtrl.js
@@ -1,3 +1,12 @@
-angular.module("rainingCatzAndDogz").controller("catCtrl", function($scope, catServ) {
+angular.module("rainingCatzAndDogz").controller("catCtrl", function($scope, catServ, $state) {
   $scope.allCats = catServ.allCats;
+  $scope.Cat = "";
+  $scope.getCat = function() {
+    for (var i = 0; i < $scope.allCats.length; i++) {
+      if ($scope.allCats[i].breed == $state.params.catId) {
+        $scope.Cat = $scope.allCats[i];
+      }
+    }
+  };
+  $scope.getCat();
 });
--- controllers/dogCtrl.js
+++ controllers/dogCtrl.js
@@ -1,3 +1,12 @@
-angular.module("rainingCatzAndDogz").controller("dogCtrl", function($scope, dogServ) {
+angular.module("rainingCatzAndDogz").controller("dogCtrl", function($scope, dogServ, $state) {
   $scope.allDogs = dogServ.allDogs;
+  $scope.Dog = "";
+  $scope.getDog = function() {
+    for (var i = 0; i < $scope.allDogs.length; i++) {
+      if ($scope.allDogs[i].breed == $state.params.dogId) {
+        $scope.Dog = $scope.allDogs[i];
+      }
+    }
+  };
+  $scope.getDog();
 });
